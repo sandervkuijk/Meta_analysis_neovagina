0b636435c6e0eab9908517ac37fd5e1b2cccf79f
Sander van Kuijk <sander1981@gmail.com>
2021-03-09 22:25:44 +0100

    Voorbereiding op data

---
+++ Meta_Linde.R
@@ -0,0 +1,10 @@
+################################################################################
+### Meta Analyse Linde, uitkomst: neovaginalengte (mean)
+### Start: 09/03/2021
+### Sander MJ van Kuijk
+################################################################################
+
+library(meta)
+
+setwd("C:/Users/sande/Documents/Werk/LINDE")
+d <- read.csv()
