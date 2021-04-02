1b5e6ad26e8566e084ceaa2b81eecc246b62c96e
Sander van Kuijk <sander1981@gmail.com>
2021-04-02 14:59:40 +0200

    Updated meta analysis code

--- Meta_Linde.R
+++ Meta_Linde.R
@@ -7,10 +7,34 @@
 library(meta)
 
 setwd("C:/Users/sande/Documents/Werk/LINDE")
-d <- read.csv()
+d <- read.csv("Davydov_neovaginalengte.csv", sep = ";")
+names(d)[1] <- "auteur"
 
-model <- metacont(n.e = , mean.e = , sd.e = , n.c = , mean.c = , sd.c = , studlab = )
+View(d)
+
+# Selectie vanaf 6 maanden?
+
+d <- d[ 10:17, ]
+
+# Email Lisanne: Fedele 2010 weglaten
+
+d <- d[-2, ]; d
+
+# SD missing:
+# d$SD[is.na(d$SD)] <- sum(d$SD*d$Aantal.patienten/
+#                             sum(d$Aantal.patienten, na.rm = TRUE), na.rm = TRUE)
+
+model <- metamean(n = Aantal.patienten, mean = Mean, sd = SD,
+                  studlab = paste(auteur, "et al.", Jaar), data = d)
+model
 
 forest(model)
+dev.off()
+
+png("forest_neovaginalengte.png", width = 700, height = 500, pointsize = 24)
+forest(model, sortvar = d$Jaar, xlab = "Length (cm)", comb.fixed = FALSE,
+       print.tau2 = FALSE, print.pval.Q = FALSE, xlim = c(6, 10),
+       smlab = "", rightlabs = c("Mean", "95% CI", "Weight"))
+dev.off()
 
 ################################################################################
